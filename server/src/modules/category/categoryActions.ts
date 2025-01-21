9d1bb3fe2715fa04ab9f0b5e6e4041cc4620ed8b
Ludovic Pouillie <ludovicpouillie@gmail.com>
2025-01-21 17:01:35 +0100

    ADD quest 06

--- server/src/modules/category/categoryActions.ts
+++ server/src/modules/category/categoryActions.ts
@@ -1,34 +1,24 @@
+import categoryRepository from "./categoryRepository";
 // Some data to make the trick
-
 const categories = [
   {
     id: 1,
-
     name: "Comédie",
   },
-
   {
     id: 2,
-
     name: "Science-Fiction",
   },
 ];
 
 // Declare the actions
-
-/* Here you code */
 import type { RequestHandler } from "express";
 
-const browse: RequestHandler = (req, res) => {
-  if (req.query.q != null) {
-    const filteredPrograms = categories.filter((categorie) =>
-      categorie.name.includes(req.query.q as string),
-    );
-    res.json(filteredPrograms);
-  } else {
-    res.json(categories);
-  }
+const browse: RequestHandler = async (req, res) => {
+  const categoriesFromDB = await categoryRepository.readAll();
+  res.json(categoriesFromDB);
 };
+
 const read: RequestHandler = (req, res) => {
   const parsedId = Number.parseInt(req.params.id);
 
